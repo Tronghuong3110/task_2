7459c7f73ca289ed68c2c94b2bb9414e9e61d963
Tronghuong3110 <tronghuong2002@gmail.com>
2023-09-06 23:08:22 +0700

    update 11h 9-6-2023

--- src/main/java/com/newlife/Connect_multiple/controller/Test.java
+++ src/main/java/com/newlife/Connect_multiple/controller/Test.java
@@ -27,19 +27,42 @@ public class Test {
 //            ProcessBuilder processBuilder = new ProcessBuilder("powershell.exe",
 //                    "Get-WmiObject Win32_Process | Where-Object { $_.CommandLine 'powershell.exe'} | Select-Object CommandLine");
 //          WMIC path win32_process get Caption,Processid,Commandline
-            ProcessBuilder processBuilder = new ProcessBuilder("powershell.exe", "WMIC path win32_process get Caption,Processid,Commandline");
+            ProcessBuilder processBuilder = new ProcessBuilder("powershell.exe",
+                                "WMIC path win32_process get Caption,Processid,Commandline");
             Process process = processBuilder.start();
 
             // Đọc kết quả từ quá trình
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
+//            Integer count = 1;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                String[] parts = line.split("\\s+");
+                if (parts.length >= 3) {
+                    String caption = parts[0];
+                    String processId = parts[parts.length-1];
+                    String commandLine = solveCommandLine(parts);
+
+                    if(commandLine.contains("ping -t 192.168.1.1")) {
+                        System.out.println("Caption: " + caption);
+                        System.out.println("Processid: " + processId);
+                        System.out.println("CommandLine: " + commandLine);
+//                        System.out.println(line);
+                        System.out.println("====================================================");
+                    }
+                }
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String solveCommandLine(String[] commandLine) {
+        String command = "";
+        for(int i = 1; i < commandLine.length-1; i++) {
+            command += commandLine[i] + " ";
+        }
+        return command;
     }
 }
 
