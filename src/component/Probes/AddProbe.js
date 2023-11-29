e50d8ee1b3fab4c6822e9671466bc50f8631c143
hungrua <canhhung592@gmail.com>
2023-11-29 10:06:34 +0700

    29-11-2023

--- src/component/Probes/AddProbe.js
+++ src/component/Probes/AddProbe.js
@@ -329,17 +329,7 @@ const AddProbe = ({ handleCloseWindow }) => {
                 </div>
             </div>)
             }
-            <ToastContainer
-                position="top-center"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="dark"></ToastContainer>
+
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
-        <App />
-    </Router>
+    <React.StrictMode>
+        <Router basename=''>
+            <App />
+        </Router>
+    </React.StrictMode>
 
 );
 
